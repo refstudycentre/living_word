72a96a0ad5448b342aecd5b245397875f0093aca
dolf <rudolfbyker@gmail.com>
2016-01-09 14:17:02 +0200

    Only execute the JS behaviour once!

--- living_word_quote/living_word_quote.js
+++ living_word_quote/living_word_quote.js
@@ -2,35 +2,42 @@
     Drupal.behaviors.livingWordQuoteBehavior = {
         attach: function (context, settings) {
 
-            // find the form elements
+            // find the form
             var form_id = Drupal.settings.living_word_quote.form_id;
             var form = $('#'+form_id+" div.fieldset-wrapper", context);
-            var form_start = form.find("input.living-word-quote-start");
-            var form_text = form.find("input.living-word-quote-text");
 
-            // get the source text
-            var source_text = Drupal.settings.living_word_quote.source_text;
+            // only do this once
+            form.once('living-word-quote-form', function() {
 
-            // hide the text fields
-            // TODO: uncomment the following line once the eye candy is done
-            //form.find('div.form-item').hide();
+                // find the form elements
+                var form_start = form.find("input.living-word-quote-start");
+                var form_text = form.find("input.living-word-quote-text");
 
-            // build the user interface
-            var textarea = $("<textarea>"+source_text+"</textarea>");
-            form.append(textarea);
+                // get the source text
+                var source_text = Drupal.settings.living_word_quote.source_text;
 
-            // act on text being selected
-            textarea.select(function (select_event) {
+                // hide the text fields
+                // TODO: uncomment the following line once the eye candy is done
+                //form.find('div.form-item').hide();
 
-                // inspect the selection
-                var position = textarea.selection('getPos');
-                var text = textarea.selection();
+                // build the user interface
+                var textarea = $("<textarea>"+source_text+"</textarea>");
+                form.append(textarea);
 
-                // fill in the form
-                form_start.val(position.start);
-                form_text.val(text);
+                // act on text being selected
+                textarea.select(function (select_event) {
 
-                // TODO: eye candy selection thingy (like on android phones)
+                    // inspect the selection
+                    var position = textarea.selection('getPos');
+                    var text = textarea.selection();
+
+                    // fill in the form
+                    form_start.val(position.start);
+                    form_text.val(text);
+
+                    // TODO: eye candy selection thingy (like on android phones)
+
+                })
 
             })
 
